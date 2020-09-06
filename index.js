310ea9c862f0f7ec7f3ad83ad1ed29dce5a3f339
scripdoc <alesis.x@mail.ru>
2020-09-07 01:41:47 +0300

    style: change indents

--- index.js
+++ index.js
@@ -23,70 +23,70 @@ app.use(express.static('./public'))
 
 
 app.get('/:id', async (req, res, next) => {
-   const { id: slug } = req.params
-   try {
-      const data = await urls.findOne({ slug })
-      
-      if (data) {
-         res.redirect(301, data.url)
-      }
-      
-      res.redirect(`/?error=${slug} not found`)
-   } catch (error) {
-      next(error)
-   }
+  const { id: slug } = req.params
+  try {
+    const data = await urls.findOne({ slug })
+
+    if (data) {
+      res.redirect(301, data.url)
+    }
+
+    res.redirect(`/?error=${slug} not found`)
+  } catch (error) {
+    next(error)
+  }
 })
 
 const schema = yup.object().shape({
-    slug: yup.string().trim().matches(/[\w\-]/i),
-    url: yup.string().trim().url().required()
+  slug: yup.string().trim().matches(/[\w\-]/i),
+  url: yup.string().trim().url().required()
 })
 
 app.post('/url', async (req, res, next) => {
-   let { slug, url } = req.body
-   try {
-      await schema.validate({
-         slug,
-         url
-      })
+  let { slug, url } = req.body
+  try {
+    await schema.validate({
+      slug,
+      url
+    })
 
-      if (!slug) {
-         slug = nanoid(7)
-      } else {
-         const existing = await urls.findOne({ slug })
-         if (existing) {
-            throw new Error('Slug is use.')
-         }
+    if (!slug) {
+      slug = nanoid(7)
+    } else {
+      const existing = await urls.findOne({ slug })
+      if (existing) {
+        throw new Error('Slug is use.')
       }
+    }
 
-      slug = slug.toLowerCase()
-      const newUrl = {
-         url,
-         slug
-      }
-      const created = await urls.insert(newUrl)
+    slug = slug.toLowerCase()
+    const newUrl = {
+      url,
+      slug
+    }
+    const created = await urls.insert(newUrl)
 
-      res.json(created)
-   } catch (error) {
-      if (!error.status) {
-         error.status = 400
-      }
-      next(error)
-   }
+    res.json(created)
+  } catch (error) {
+    if (!error.status) {
+      error.status = 400
+    }
+    next(error)
+  }
 })
 
 app.use((error, req, res, next) => {
-   if (error.status) {
-      res.status(error.status)
-   } else {
-      res.status(500)
-   }
-   res.json({
-      message: error.message,
-      stack: process.env.NODE_ENV === 'production' ? 'ok' : error
-   })
+  if (error.status) {
+    res.status(error.status)
+  } else {
+    res.status(500)
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? 'ok' : error
+  })
 })
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`)
 })
