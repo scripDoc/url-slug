77b6c5cc1b7a226c3b15a0e99eb2045ae89cf7b9
scripdoc <alesis.x@mail.ru>
2020-09-06 23:40:28 +0300

    feat: create api

--- index.js
+++ index.js
@@ -2,20 +2,92 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const morgan = require('morgan')
+const yup = require('yup')
+const monk = require('monk')
+const { nanoid } = require('nanoid')
+
+require('dotenv').config()
+
+const db = monk(process.env.MONGO_URI)
+const urls = db.get('urls')
+urls.createIndex({ slug: 1 }, { unique: true })
 
 const app = express()
-const port = process.env.PORT || 4200
+const port = process.env.PORT || 4242
 
 app.use(helmet())
-app.use(morgan('tiny'))
+app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'All right'
-    })
+app.get('/url/:id', (req, res, next) => {
+   // TODO: get a short url by id
+})
+
+app.get('/:id', async (req, res) => {
+   const { id: slug } = req.params
+   try {
+      const data = await urls.findOne({ slug })
+      
+      if (data) {
+         res.redirect(301, data.url)
+      }
+      
+      res.redirect(`/?error=${slug} not found`)
+   } catch (error) {
+      next(error)
+   }
+})
+
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required()
+})
+
+app.post('/url', async (req, res, next) => {
+   let { slug, url } = req.body
+   try {
+      await schema.validate({
+         slug,
+         url
+      })
+
+      if (!slug) {
+         slug = nanoid(7)
+      } else {
+         const existing = await urls.findOne({ slug })
+         if (existing) {
+            throw new Error('Slug is use.')
+         }
+      }
+
+      slug = slug.toLowerCase()
+      const newUrl = {
+         url,
+         slug
+      }
+      const created = await urls.insert(newUrl)
+
+      res.json(created)
+   } catch (error) {
+      if (!error.status) {
+         error.status = 400
+      }
+      next(error)
+   }
+})
+
+app.use((error, req, res, next) => {
+   if (error.status) {
+      res.status(error.status)
+   } else {
+      res.status(500)
+   }
+   res.json({
+      message: error.message,
+      stack: process.env.NODE_ENV === 'production' ? 'ok' : error
+   })
 })
 
 app.listen(port, () => {
